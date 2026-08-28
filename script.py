ee324e3fe4a34237bb154c679fe946f9b45ed541
antonio <antoniocamposcesar2008@gmail.com>
2026-08-27 21:29:48 -0300

    adicionando o menu

--- script.py
+++ script.py
@@ -1,2 +1,7 @@
 from InquirerPy import inquirer
 
+escolha= inquirer.select(
+    message="escolha sua ação",
+    choices=["organizador de arquivos", "pedra, papel e tesoura","revisar o clima"]
+).execute()
+
