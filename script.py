dc6504e67b9f5de85d9d68e043e78c53f401cbf2
antonio <antoniocamposcesar2008@gmail.com>
2026-08-28 22:27:19 -0300

    listtar

--- script.py
+++ script.py
@@ -3,17 +3,51 @@ import os
 import random
 from pathlib import Path
 import shutil
+import json
 
+ARQUIVOS_TAREFAS= "tarefas.json"
+def ler_tarefas():
+    if not os.path.exists(ARQUIVOS_TAREFAS):
+        return []
+    with open(ARQUIVOS_TAREFAS, "r") as arquivo:
+        return json.load(arquivo)
+def salvar_tarefas(tarefas):
+    with open (ARQUIVOS_TAREFAS, "w") as arquivo:
+        json.dump(tarefas, arquivo, indent= 4, ensure_ascii= False)
+def inserir_tarefas(id_tarefa, nome_tarefa):
+    tarefas= ler_tarefas()
+    nova_tarefa={
+        "id": id_tarefa,
+        "tarefa": nome_tarefa,
+        "concluida": False
+    }
+    tarefas.append(nova_tarefa)
+    salvar_tarefas(tarefas)
+    print(f"Tarefa {nome_tarefa} inserido com sucesso")
+def excluir_tarefa(id_tarefa):
+    tarefas= ler_tarefas()
+    tarefas_atualizadas= [t for t in tarefas if t["id"] != id_tarefa]
+    if len(tarefas)== len(tarefas_atualizadas):
+        print(f"Aviso: Nenhuma tarefa encontrada com o ID {id_tarefa}.")
+    else:
+        salvar_tarefas(tarefas_atualizadas)
+        print(f"tarefa com ID {id_tarefa} excluida com sucesso!")
+
+
+
+
+
+
+
+os.system("clear")
 
 escolha= inquirer.select(
     message="escolha sua ação",
-    choices=["organizador de arquivos", "pedra, papel e tesoura","revisar o clima"]
+    choices=["organizador de arquivos", "pedra, papel e tesoura","lista de tarefas"]
 ).execute()
 
-
-
 if escolha== "organizador de arquivos":
-    print("")
+    
     #pasta que queremos organizar
     pasta= Path.home()/ "Downloads"
     #especificação dos arquivos
@@ -74,5 +108,34 @@ if escolha=="pedra, papel e tesoura":
             # se ele perder ele printa o voce perdeu 
  
 
-if escolha=="revisar o clima":
-    print("r. clima")
+if escolha=="lista de tarefas":
+    os.system("clear")
+
+    es= inquirer.select(message="=== Menu de oções ===", choices= ["ler tarefa", "Criar uma tarefa", "Excluir/Concluir uma tarefa", "sair"]).execute()
+    if es == "ler tarefa":
+        tare= ler_tarefas()
+        if not tare:
+            print("nenhuma tarefa registrada")
+            
+        else: 
+            for t in tare:
+                status= "feito" if t["concluida"] else "nao feito"
+                print(f"[{t['id']}] {t["tarefa"]}- status: {status}")
+
+
+    if es == "Criar uma tarefa":
+        a= input("diga sua tarefa: ")
+        b= int(input("diga a posição dela: "))
+
+        inserir_tarefas(b,a)
+        print(f"tarefa {a} inserida com sucesso na posição {b}")
+
+    if es == "Excluir/Concluir uma tarefa":
+        c= int(input("escreva qual a posição dela: "))
+        excluir_tarefa(c)
+        print("exclusão feita")
+
+    if es== "sair":
+        print("projeto desenvolvido por: Antonio César")
+
+
